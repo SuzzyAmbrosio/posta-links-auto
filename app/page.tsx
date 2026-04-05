6b1f785322d49771fb7d84f2fca8cc22bf868a57
SuzzyAmbrosio <suzelly.ambrosio@gmail.com>
2026-04-05 04:11:22 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>SaaS Links</h1>
+      <p>Funcionando 🚀</p>
+      <a href="/dashboard">Ir para dashboard</a>
+    </main>
+  );
+}
