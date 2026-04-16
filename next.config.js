269f6411fc3df818454a57d6c1dff561a1789a5a
SuzzyAmbrosio <suzelly.ambrosio@gmail.com>
2026-04-15 22:57:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  staticPageGenerationTimeout: 1000,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
